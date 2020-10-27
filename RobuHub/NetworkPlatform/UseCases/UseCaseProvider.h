fe8006d298a3b6f1501dfe74b737c002bd680d67
KarimEbrahem <karimabdelazizmansour@gmail.com>
2020-10-27 07:14:05 +0200

    Add concrete implementation for use case provider for Network Platform

---
+++ RobuHub/NetworkPlatform/UseCases/UseCaseProvider.h
@@ -0,0 +1,12 @@
+//
+//  UseCaseProvider.h
+//  NetworkPlatform
+//
+//  Created by KarimEbrahem on 10/27/20.
+//
+
+#import "Domain/UseCaseProvider.h"
+
+@interface UseCaseProvider: NSObject <UseCaseProvider>
+
+@end
